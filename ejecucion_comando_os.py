3c67738691c6a1ee2579bdc490119e36e99eebc6
zetikarthack <114775443+zetikart@users.noreply.github.com>
2024-12-07 22:30:07 -0300

    Create ejecucion_comando_os.py
    🖥️ Ejecución de Comandos del Sistema en Python 🐍
    ¡Bienvenido a mi repositorio! 🚀 Aquí encontrarás un sencillo script de Python que muestra cómo ejecutar comandos del sistema utilizando el módulo os y verificar el resultado. 📁
    
    
    🎯 Funcionalidades
    🖥️ Ejecutar comandos del sistema con os.system()
    
    ✅ Verificar el valor de retorno para confirmar la ejecución exitosa
    
    🌐 Contribuciones
    ¡Las contribuciones son bienvenidas! Si tienes alguna idea o mejora, siéntete libre de abrir un issue o enviar un pull request. 🤝

---
+++ ejecucion_comando_os.py
@@ -0,0 +1,7 @@
+import os
+
+# Ejecutar el comando del sistema para crear un directorio llamado "my_first_directory"
+returned_value = os.system("mkdir my_first_directory")
+
+# Imprimir el valor retornado por el comando del sistema
+print(returned_value)
